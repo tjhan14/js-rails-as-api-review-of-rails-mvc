6e7b3fbdb5f135c96923b58c1e5822d44532dcde
Taek Jin Han <tj.han@yale.edu>
2021-08-09 17:15:38 -0400

    done

--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,1 +1,13 @@
 # Add code from Readme
+
+ActiveRecord::Schema.define(version: 2019) do
+
+    create_table "birds", force: :cascade do |t|
+      t.string "name"
+      t.string "species"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+  
+  end
+  
